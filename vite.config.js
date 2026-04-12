695238a21d7e7927032643737cafd32140344147
Deathlimit <101141306+Deathlimit@users.noreply.github.com>
2026-04-12 18:24:09 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TestingExpertSystem/',
+  base: '/Expert_system_backend/',
   server: {
     port: 3000,
     proxy: {
